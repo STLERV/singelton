2cccbbf72bc24097677cb0d36dfb76ee2a8ebbe2
lcadena <36576978+lcadena@users.noreply.github.com>
2018-03-07 17:05:33 +0100

    tercer commit con maven

--- src/main/java/I18NManager.java
+++ src/main/java/I18NManager.java
@@ -1,5 +1,3 @@
-package main.java;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -7,20 +5,35 @@ import java.util.ResourceBundle;
 public class I18NManager {
     private static I18NManager instance;
     private HashMap<String,ResourceBundle> data;
-    data.put("T1",)
+
+    private I18NManager(){
+        data = new HashMap<String, ResourceBundle>();
+    }
 
     public HashMap<String, ResourceBundle> getData() {
         return data;
     }
 
     public static I18NManager getInstance(){
-        if (instance==null) instance = new I18NManager;
+        if (instance==null) instance = new I18NManager();
         return instance;
     }
-    private HashMap<String,ResourceBundle> data;
 
-    public I18NManager data(){
-        ResourceBundle rb = data.get()
+    public String getText(String lenguaje, String key){
+        ResourceBundle rb = data.get(lenguaje);
+        if(rb==null){
+            rb = ResourceBundle.getBundle("edu.uc.eetac.dsa.patrons.i18n "+lenguaje);
+            data.put(lenguaje, rb);
+        }
+        return rb.getString(key);
+    }
+
+
+    public static void main(String[] args) {
+        // write your code here
+        String value = I18NManager.getInstance().getText("cas", "t1");
+        value = I18NManager.getInstance().getText("cat", "t2");
+        value = I18NManager.getInstance().getText("en", "t3");
     }
 
 }
